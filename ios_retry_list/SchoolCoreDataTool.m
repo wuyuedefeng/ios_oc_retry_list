867cc03a415d5ff4ecc47d198bb384ad05d50922
wangsen <wangs@tongxinyiliao.com>
2016-01-10 21:48:25 +0800

    core data  delete search  method

--- ios_retry_list/SchoolCoreDataTool.m
+++ ios_retry_list/SchoolCoreDataTool.m
@@ -20,7 +20,12 @@
 + (void)testCompanyCoreData{
     SchoolCoreDataTool *schoolCoreDataTool = [[SchoolCoreDataTool alloc] init];
     [schoolCoreDataTool setupContext];
-    [schoolCoreDataTool readStudents];
+    [schoolCoreDataTool addStudent];
+//    [schoolCoreDataTool readStudents];
+//    [schoolCoreDataTool deleteStudentWithName:@"wangyi"];
+    
+//    [schoolCoreDataTool likeSearcher];
+    [schoolCoreDataTool searchStudentWithName:@"wangyi"];
     
     
 }
@@ -63,6 +68,89 @@
     }else{
         NSLog(@"%@",error);
     }
+}
+
+# pragma mark 删除student
+- (void)deleteStudentWithName:(NSString *)name{
+    // 删除zhangsan
+    // 1.查找到zhangsan
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Student"];
+    
+    NSPredicate *pre = [NSPredicate predicateWithFormat:@"name=%@",name];
+    request.predicate = pre;
+    
+    // 2.删除 匹配到姓名的学生
+    NSArray *students = [_context executeFetchRequest:request error:nil];
+    
+    for (Student *student in students) {
+        NSLog(@"删除学生 %@",student.name);
+        [_context deleteObject:student];
+        
+        //可以同时删除关系表中的老师
+        //[_context deleteObject:student.teacher];
+    }
+    
+    // 3.用context同步下数据库
+    //所有的操作暂时都是在内存里，调用save 同步数据库
+    [_context save:nil];
+}
+
+#pragma mark 模糊查询
+- (NSArray *)searchStudentWithName:(NSString *)name{
+    
+    // 1.查找员工
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Student"];
+    
+    NSPredicate *pre = [NSPredicate predicateWithFormat:@"name=%@",name];
+    request.predicate = pre;
+    
+    NSError *error = nil;
+    NSArray *students = [_context executeFetchRequest:request error:&error];
+    
+    if (!error) {
+        NSLog(@"students: %@",students);
+        for (Student *student in students) {
+            NSLog(@"search --> %@ %@ %@",student.name,student.age,student.teacher.name);
+        }
+    }else{
+        NSLog(@"%@",error);
+    }
+    
+    return students;
+}
+
+#pragma mark 模糊查询
+- (void)likeSearcher {
+    
+    // 查询
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Student"];
+    
+    // 过滤
+    // 1.查询以wang开头学生
+    //NSPredicate *pre = [NSPredicate predicateWithFormat:@"name BEGINSWITH %@",@"wang"];
+    
+    // 2.以si 结尾
+    //NSPredicate *pre = [NSPredicate predicateWithFormat:@"name ENDSWITH %@",@"si"];
+    
+    // 3.名字包含 g
+    //NSPredicate *pre = [NSPredicate predicateWithFormat:@"name CONTAINS %@",@"g"];
+    
+    // 4.like 以si结尾
+    NSPredicate *pre = [NSPredicate predicateWithFormat:@"name like %@",@"wang*"];
+    request.predicate = pre;
+    
+    //读取信息
+    NSError *error = nil;
+    NSArray *students = [_context executeFetchRequest:request error:&error];
+    if (!error) {
+        NSLog(@"students: %@",students);
+        for (Student *student in students) {
+            NSLog(@"like_search --> %@ %@ %@",student.name,student.age,student.teacher.name);
+        }
+    }else{
+        NSLog(@"%@",error);
+    }
+
 }
 
 
